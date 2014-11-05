ea51825f1653ae442775ff3e8c0604a72487ddbe
eddy <eddyxu1213@126.com>
2014-11-04 22:10:07 -0800

    Python学习 序列

---
+++ org/eddy/python/seq.py
@@ -0,0 +1,20 @@
+__author__ = 'root'
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+
+print 'Item 0 is', shoplist[0]
+print 'Item 1 is', shoplist[1]
+print 'Item 2 is', shoplist[2]
+print 'Item 3 is', shoplist[3]
+print 'Item -1 is', shoplist[-1]
+print 'Item -2 is', shoplist[-2]
+
+print 'Item 1 to 3 is', shoplist[1: 3]
+print 'Item 2 to end is', shoplist[2:]
+print 'Item 1 to -1 is', shoplist[1: -1]
+print 'Item start to end is', shoplist[:]
+
+name = 'swaroop'
+print 'char 1 to 3 is', name[1: 3]
+print 'char 2 to  end is', name[2:]
+print 'char 1 to -1 is ', name[1: -1]
+print 'char start to end is ', name[:]
